b68c8cec4cbb74b88a4eb7b7399d01aaa793da2c
IamAnNguyeenX21 <nguyenan0979@gmail.com>
2026-03-31 17:03:59 +0700

    refactor: remove unnecessary struct type

--- routing_table.h
+++ routing_table.h
@@ -11,15 +11,7 @@ typedef struct routing_information
     struct routing_information* next;
 } routing_information;
 
-typedef struct topic_entry
-{
-    int topic_id;
-    routing_information** head;
-    pthread_mutex_t mutex;
-} topic_entry;
-
 void print_list(routing_information* head);
-int topic_entry_init(topic_entry* entry, routing_information** head, int topic_id);
 void add_last(routing_information** head, int client_fd);
 void delete_node(routing_information** head, int client_fd);
 void check_and_add(routing_information** head, int client_fd);
